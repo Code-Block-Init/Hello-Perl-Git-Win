705b7f7f10d8da42f457cbccf9639fc4baf8a481
Aswini S <ashumeow@live.com>
2015-06-30 03:46:12 +0530

    add arithmetic operations

---
+++ getting-started/arithmetic_operations.pl
@@ -0,0 +1,17 @@
+use warnings;
+use strict;
+my $x;
+my $y;
+print "enter value of x\n";
+$x=<STDIN>;
+print "enter value of y\n";
+$y=<STDIN>;
+print "addition:\n";
+print $x+$y; # addition
+print "\nsubtraction:\n";
+print $x-$y; # subtraction
+print "\nmultiplication:\n";
+print $x*$y; #multiplication
+print "\ndivision:\n";
+print $x/$y; #division
+print "\nbye bye! =P"
