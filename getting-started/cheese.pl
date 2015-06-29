93df311ab4f93c7257140122926725bc9b9ee3ad
Aswini S <ashumeow@live.com>
2015-06-30 03:05:33 +0530

    Update cheese.pl

--- getting-started/cheese.pl
+++ getting-started/cheese.pl
@@ -8,11 +8,13 @@ use Scalar::Util qw(looks_like_number);
 
 # display
 print "Do you love cheese?\n";
+print "type 1 for yes\n";
+print "type 0 for no\n";
 my $buy=<STDIN>;
 chomp $buy;
 
 # looping
-if(looks_like_number($buy)) {
+if(looks_like_number($buy==1)) {
 	print "Aww... Let's make cheesy sandwich <3 \n";
 } else {
 	print "OK. How about some peanut butter?";
