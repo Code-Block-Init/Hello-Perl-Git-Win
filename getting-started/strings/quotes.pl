ed213a4091ef5061741ab1cc5bc990f15718bfba
Aswini S <ashumeow@live.com>
2015-07-01 02:06:57 +0530

    add quotations

---
+++ getting-started/strings/quotes.pl
@@ -0,0 +1,14 @@
+use strict; # optional to add this line
+use warnings; # optional to add this line
+
+# init
+my $name='Ashu';
+
+# single quotes
+print 'Hello $name! You want cheese?\n';
+
+print "\n"; # adding a line space
+
+# double quotes
+print "Hello $name! You want cheese?\n";
+# fini ;)
