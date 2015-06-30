79114e921d2c70c1fbe8620a9a695f4b0bf01bf3
Aswini S <ashumeow@live.com>
2015-06-30 21:53:12 +0530

    add increment

---
+++ getting-started/strings/increment.pl
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+
+# declaration
+my $x="ig";
+
+# increment - 1
+$x++;
+print $x; # display
+print "\n"; # add a line space
+
+# increment - 2
+$x++;
+print $x; # display
+print "\n"; # add a line space
+
+# increment - 3
+$x++;
+print $x; # display
