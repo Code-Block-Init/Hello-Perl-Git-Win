1473dd994cefdf81f3b341640a9709e9f439bdaf
Aswini S <ashumeow@live.com>
2015-06-30 05:58:28 +0530

    add basic strings

---
+++ getting-started/strings/basic.pl
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+
+# declaration
+my $x='Hello';
+my $y='Meow!';
+
+# strings
+my $z1=$x.''.$y; # without space
+my $z2=$x.' '.$y; # with space
+
+# display
+print $z1;
+print "\n"; # adding a line space
+print $z2;
