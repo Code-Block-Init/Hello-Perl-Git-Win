f98bf35db522f1fdb5b4c67e8efacb278fe10ff0
Aswini S <ashumeow@live.com>
2015-07-01 03:27:34 +0530

    add email

---
+++ getting-started/strings/email.pl
@@ -0,0 +1,21 @@
+use strict;
+use warnings;
+
+################################
+# broken email
+#my $ashu = "ashumeow@live.com";
+#print $ashu;
+################################
+#print "\n";
+
+# appropriate way
+my $aswini = 'aswini1405@outlook.com';
+print $aswini; # displays email id
+
+#print "\n"
+
+########################################
+#my $ashumeow='ashumeow';
+#my $ashumeow5="$ashumeow\@live.com";
+#print $ashumeow5;
+#########################################
