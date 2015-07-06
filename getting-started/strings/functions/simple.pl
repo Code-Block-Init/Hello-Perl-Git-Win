1d24efd028839491c5c0bb71914109ff3274dc05
Aswini S <ashumeow@live.com>
2015-07-06 08:00:04 +0530

    add functions

---
+++ getting-started/strings/functions/simple.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+use 5.022;
+
+# init
+my $user='AshuMeow';
+
+# lc (lowercase), uc (uppercase), length
+print lc $user;
+print "\n";
+print uc $user;
+print "\n";
+print length $user;
