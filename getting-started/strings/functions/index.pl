02487da97fcb3ecbf0af253233de3d1758db35e3
Aswini S <ashumeow@live.com>
2015-07-06 08:39:26 +0530

    add index

---
+++ getting-started/strings/functions/index.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+use 5.022;
+
+my $mnc = "Companies use and throw for money";
+
+print index $mnc, 'heartless';
+print "\n";
+print index $mnc, 'merciless';
+print "\n";
+print index $mnc, 'destroyers';
+print "\n";
+print index $mnc, 'betrayers';
+#fini
