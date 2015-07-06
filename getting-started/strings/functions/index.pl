162114c1ddf4db14eff687f3c237254fe3704269
Aswini S <ashumeow@live.com>
2015-07-06 09:25:05 +0530

    Update index.pl

--- getting-started/strings/functions/index.pl
+++ getting-started/strings/functions/index.pl
@@ -4,6 +4,7 @@ use 5.022;
 
 my $mnc = "Companies use and throw for money";
 
+# index - single quotes
 print index $mnc, 'heartless';
 print "\n";
 print index $mnc, 'merciless';
@@ -13,4 +14,22 @@ print "\n";
 print index $mnc, 'betrayers';
 print "\n";
 print index $mnc, 'use and throw';
+
+print "\n\n";
+
+# index - double quotes
+print index $mnc, "e ";
+print "\n";
+print index $mnc, "e ";
+print "\n";
+print index $mnc, "e";
+
+print "\n\n";
+
+# rindex
+print rindex $mnc, "e ";
+print "\n";
+print rindex $mnc, "e ";
+print "\n";
+print rindex $mnc, "e";
 #fini
