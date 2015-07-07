fd6dfc314380d47333d0a0ad099760ca16946341
Aswini S <ashumeow@live.com>
2015-07-07 16:53:10 +0530

    Update sort.pl

--- getting-started/sort.pl
+++ getting-started/sort.pl
@@ -2,14 +2,29 @@ use strict;
 use warnings;
 use 5.022;
 
+print "Sorting of 5 numbers\n";
 my $x;
+print "Enter the 1st value: \n";
+$x=<STDIN>;
 my $y;
+print "Enter the 2nd value: \n";
+$y=<STDIN>;
+my $c;
+print "Enter the 3rd value: \n";
+$c=<STDIN>;
+my $d;
+print "Enter the 4th value: \n";
+$d=<STDIN>;
+my $e;
+print "Enter the 5th value: \n";
+$e=<STDIN>;
 
-my @sorted;
-my @strings;
+my @sorting=($x,$y,$c,$d,$e);
 
-@sorted=sort {
-	length $x <=> length $y
-	or
-	$x cmp $y
-} @strings;
+@sorting=sort {
+	$a<=>$b # ascending order
+} $x,$y,$c,$d,$e;
+
+print "\n";
+
+print "The sorted numbers are:\n@sorting\n";
