8accdcf33eaf106cd96f46959ed558f1f02c6716
Aswini S <ashumeow@live.com>
2015-07-07 14:04:04 +0530

    Update arrays_multi.pl

--- getting-started/arrays_multi.pl
+++ getting-started/arrays_multi.pl
@@ -21,3 +21,15 @@ print "\n";
 # visualizing the multi-dimensional array
 use Data::Dumper qw(Dumper);
 print Dumper \@matrix;
+
+print "\n";
+
+# more than 2 dimensions
+my @matrix1;
+$matrix1[1][3][0]='ashu-meow';
+$matrix1[1][3][1]='meow-meow';
+
+print "$matrix1[1][3][0]\n";    
+print "$matrix1[1][3][1]\n";
+print"\n";
+print Dumper \@matrix1;
