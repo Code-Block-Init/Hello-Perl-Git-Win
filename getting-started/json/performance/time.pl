31664c7d346ccac536406069ea19d971965c5574
Aswini S <ashumeow@live.com>
2015-07-08 13:57:00 +0530

    Update time.pl

--- getting-started/json/performance/time.pl
+++ getting-started/json/performance/time.pl
@@ -4,6 +4,7 @@ use 5.022;
  
 use JSON();
 use JSON::XS();
+use JSON::PP();
 use Benchmark qw(cmpthese);
  
 my $json = create_json(100);
@@ -11,6 +12,7 @@ my $json = create_json(100);
 cmpthese(10000, {
     'JSON'     => sub { JSON::decode_json($json) },
     'JSON::XS' => sub { JSON::XS::decode_json($json) },
+    'JSON::PP' => sub { JSON::PP::decode_json($json) },
 });
  
 sub create_json {
