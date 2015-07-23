ed46b04490ee607d4a3484ea0e6f76e6804b4d10
Aswini S <ashumeow@live.com>
2015-07-23 07:44:01 +0530

    add reverse string and number

---
+++ getting-started/strings/reverse.pl
@@ -0,0 +1,24 @@
+# reversing a string or a number
+use strict;
+use warnings;
+use 5.022;
+
+my $string="headache";
+print $string;
+
+print "\n";
+
+# reversing the $string
+my $reverse_str=reverse $string;
+print $reverse_str;
+
+print "\n";
+
+my $number=3.14;
+print $number;
+
+print "\n";
+
+# reversing the number
+my $reverse_num=reverse $number;
+print $reverse_num;
